2754f0a485e78c5f3d9ecb38c340bc4758e88e13
Nikita <54820804+iamn1k@users.noreply.github.com>
2020-07-18 16:37:05 +0300

    add comments to output result
    add comments to output with the expected result

--- use.py
+++ use.py
@@ -8,10 +8,10 @@ streetlights = np.array( [[ 1, 0, 1 ],
 walk_vs_stop = np.array([[ 1, 1, 0, 0]]).T                
 model = StreetlightsNeuroWeb(streetlights,walk_vs_stop)
 model.fit(121,alpha=0.05)
-print(model.predict_light([0,0,0]))
-print(model.predict_light([0,1,0]))
-print(model.predict_light([0,1,1]))
-print(model.predict_light([1,0,0]))
-print(model.predict_light([1,0,1]))
-print(model.predict_light([1,1,0]))
-print(model.predict_light([1,1,1]))
+print(model.predict_light([0,0,0]))# False
+print(model.predict_light([0,1,0]))# True
+print(model.predict_light([0,1,1]))# True
+print(model.predict_light([1,0,0]))# False
+print(model.predict_light([1,0,1]))# True
+print(model.predict_light([1,1,0]))# False
+print(model.predict_light([1,1,1]))# True
